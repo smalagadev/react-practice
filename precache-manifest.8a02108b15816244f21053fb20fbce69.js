0175dadaf9f667a846ca3ad27c57b9f297801882
Steffano <smalagadev@gmail.com>
2019-10-10 14:40:38 -0400

    Updates

---
+++ precache-manifest.8a02108b15816244f21053fb20fbce69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e620b3083902af0f6fd89952fedbe",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "bd75696b1f89c6ea3442",
+    "url": "/react-practice/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6013becb82b8a9183dd7",
+    "url": "/react-practice/static/js/2.4c45f692.chunk.js"
+  },
+  {
+    "revision": "bd75696b1f89c6ea3442",
+    "url": "/react-practice/static/js/main.5d88e5ba.chunk.js"
+  },
+  {
+    "revision": "e4dba72985f3b5a97bd6",
+    "url": "/react-practice/static/js/runtime-main.6d42f834.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-practice/static/media/logo.25bf045c.svg"
+  }
+]);
